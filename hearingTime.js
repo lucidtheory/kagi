3e5236efd73acc25b15782fc2733bab586c6225f
Esdras Etrenne <es.etrenne@gmail.com>
2025-03-04 15:57:55 -0600

    add comment

--- hearingTime.js
+++ hearingTime.js
@@ -23,5 +23,10 @@ const start = performance.now();
 const time = hearingEndTime("Zane", 1, "Mark Hank Ana Vivian");
 const end = performance.now();
 
+/**
+ * --- With above inputs --
+ * Time for hearing: 150
+ * Time to run: 0.07072700000000154
+ */
 console.log("Time for hearing:", time);
 console.log("Time to run:", end - start);

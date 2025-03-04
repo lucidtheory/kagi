f1b5b1131b43f98b4813a2bb0c88bc89f9df9f26
Esdras Etrenne <es.etrenne@gmail.com>
2025-03-04 15:56:28 -0600

    first commit

---
+++ hearingTime.js
@@ -0,0 +1,27 @@
+const TIME_PER_HEARING = 30;
+
+const hearingEndTime = (
+    myName,
+    numJudges,
+    otherPeople
+) => {
+    return Math.ceil(
+        ((
+            // Append myName to people (string interpolation was slower)
+            otherPeople + ' ' + myName
+        // sort and find index of name, and 1 for total people
+        ).split(' ')
+        .sort()
+        .indexOf(myName) + 1)
+        // Divide inclusive total by number of judges
+        / numJudges
+    // Math.ceil for total hearing blocks * time per hearing
+    ) * TIME_PER_HEARING
+}
+
+const start = performance.now();
+const time = hearingEndTime("Zane", 1, "Mark Hank Ana Vivian");
+const end = performance.now();
+
+console.log("Time for hearing:", time);
+console.log("Time to run:", end - start);
